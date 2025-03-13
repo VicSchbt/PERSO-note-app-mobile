d8be10c326141aa8d999e164e9960b420f1df0b8
VicSchbt <victoire.schubert@gmail.com>
2025-03-13 19:18:49 +0100

    feat(list): add EmptyState for AllNotesScreen and ArchivesScreen

--- app/src/main/java/com/example/noteapp/ui/screens/all_notes_screen/AllNotesScreen.kt
+++ app/src/main/java/com/example/noteapp/ui/screens/all_notes_screen/AllNotesScreen.kt
@@ -1,17 +1,27 @@
 package com.example.noteapp.ui.screens.all_notes_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.noteapp.data.model.Note
 import com.example.noteapp.ui.composables.NoteList
 import com.example.noteapp.ui.composables.ScreenTitle
+import com.example.noteapp.ui.theme.Neutral100
+import com.example.noteapp.ui.theme.Neutral200
+import com.example.noteapp.ui.theme.Neutral950
 import com.example.noteapp.ui.theme.NoteAppTheme
 
 @Composable
@@ -31,7 +41,34 @@ fun AllNotesScreenContent(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         ScreenTitle("All notes")
-        NoteList(notes = notes, onNoteClick = { onNoteClick(it) })
+        if (notes.isNotEmpty()) {
+            NoteList(notes = notes, onNoteClick = { onNoteClick(it) })
+        } else {
+            EmptyState()
+        }
+
+    }
+}
+
+@Composable
+private fun EmptyState() {
+    Box(
+        modifier = Modifier
+            .background(Neutral100, shape = RoundedCornerShape(8.dp))
+            .border(
+                width = 1.dp,
+                color = Neutral200,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(8.dp)
+    ) {
+        Text(
+            "You don’t have any notes yet. Start a new note to capture your thoughts and ideas.",
+            fontSize = 14.sp,
+            lineHeight = 16.sp,
+            fontWeight = FontWeight.Normal,
+            color = Neutral950
+        )
     }
 }
 
@@ -44,6 +81,19 @@ fun AllNotesScreenPreview() {
             Note(id = 2, title = "Workout", text = "Go for a run"),
             Note(id = 3, title = "Study", text = "Learn Compose basics")
         )
+
+        AllNotesScreenContent(
+            notes = sampleNotes,
+            onNoteClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AllNotesScreenPreviewEmptyState() {
+    NoteAppTheme {
+        val sampleNotes = emptyList<Note>()
 
         AllNotesScreenContent(
             notes = sampleNotes,
--- app/src/main/java/com/example/noteapp/ui/screens/archives/ArchivesScreen.kt
+++ app/src/main/java/com/example/noteapp/ui/screens/archives/ArchivesScreen.kt
@@ -1,16 +1,28 @@
 package com.example.noteapp.ui.screens.archives
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.noteapp.data.model.Note
 import com.example.noteapp.ui.composables.NoteList
 import com.example.noteapp.ui.composables.ScreenTitle
+import com.example.noteapp.ui.theme.Neutral100
+import com.example.noteapp.ui.theme.Neutral200
+import com.example.noteapp.ui.theme.Neutral950
+import com.example.noteapp.ui.theme.NoteAppTheme
 
 @Composable
 fun ArchivesScreen(viewModel: ArchivesViewModel, onNoteClick: (noteId: Int) -> Unit) {
@@ -29,6 +41,69 @@ fun ArchivesScreenContent(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         ScreenTitle("Archived Notes")
-        NoteList(notes = notes, onNoteClick = { onNoteClick(it) })
+        if (notes.isNotEmpty()) {
+            NoteList(notes = notes, onNoteClick = { onNoteClick(it) })
+        } else {
+            EmptyState()
+        }
+    }
+}
+
+@Composable
+private fun EmptyState() {
+    Text(
+        "All your archived notes are stored here. You can restore or delete them anytime.",
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+        color = Neutral950
+    )
+    Box(
+        modifier = Modifier
+            .background(Neutral100, shape = RoundedCornerShape(8.dp))
+            .border(
+                width = 1.dp,
+                color = Neutral200,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(8.dp)
+    ) {
+        Text(
+            "No notes have been archived yet. Move notes here for safekeeping, or create a new note.",
+            fontSize = 14.sp,
+            lineHeight = 16.sp,
+            fontWeight = FontWeight.Normal,
+            color = Neutral950
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WithNotes() {
+    NoteAppTheme {
+        val sampleNotes = listOf(
+            Note(id = 1, title = "Buy groceries", text = "Milk, Eggs, Bread"),
+            Note(id = 2, title = "Workout", text = "Go for a run"),
+            Note(id = 3, title = "Study", text = "Learn Compose basics")
+        )
+
+        ArchivesScreenContent (
+            notes = sampleNotes,
+            onNoteClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AEmptyState() {
+    NoteAppTheme {
+        val sampleNotes = emptyList<Note>()
+
+        ArchivesScreenContent(
+            notes = sampleNotes,
+            onNoteClick = {}
+        )
     }
 }
